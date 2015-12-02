61ec0d07b701c34b13b8f6c39031c192e87c8e94
agdt3 <pavel.a@aol.com>
2015-12-01 23:50:15 -0500

    added cookie and tests

---
+++ cookie.go
@@ -0,0 +1,160 @@
+package sessioncookie
+
+import (
+    "io"
+    "time"
+    "errors"
+    "strings"
+    "crypto/aes"
+    "crypto/rand"
+    "crypto/hmac"
+    "crypto/cipher"
+    "crypto/sha256"
+    "encoding/base64"
+)
+
+// TODO: Fewer panics, more errors
+func concatValue(hash_mac string, timestamp string, data string, sep string) string {
+    if sep == "" {
+        sep = "."
+    }
+    return hash_mac + sep+ timestamp + sep + data
+}
+
+func encryptData(secret_key []byte, data []byte) []byte {
+    // add padding to block size
+    remainder := len(data) % aes.BlockSize
+    if remainder != 0 {
+        padding_length := aes.BlockSize - remainder
+        padding := ""
+        for i := 0; i < padding_length; i++ {
+            padding += "="
+        }
+        data = append(data, padding...)
+    }
+
+    block, err := aes.NewCipher(secret_key)
+    if err != nil {
+        panic(err)
+    }
+
+    // empty array of size block + data
+    ciphertext := make([]byte, aes.BlockSize+len(data))
+
+    // iv is appended to ciphertext
+    iv := ciphertext[:aes.BlockSize]
+
+    // reads from rand.Reader global singleton PRNG into iv
+    _, err = io.ReadFull(rand.Reader, iv)
+    if err != nil {
+        panic(err)
+    }
+
+    // data is encrypted
+    mode := cipher.NewCBCEncrypter(block, iv)
+    mode.CryptBlocks(ciphertext[aes.BlockSize:], data)
+
+    return ciphertext
+}
+
+
+func decryptData(secret_key []byte, encrypted_data []byte) string {
+    block, err := aes.NewCipher(secret_key)
+    if err != nil {
+        panic(err)
+    }
+
+    if len(encrypted_data) < aes.BlockSize {
+        panic("text too short")
+    }
+
+    iv := encrypted_data[:aes.BlockSize]
+    data := encrypted_data[aes.BlockSize:]
+
+    if len(data) % aes.BlockSize != 0 {
+        panic("data is not a multiple of block size")
+    }
+
+    mode := cipher.NewCBCDecrypter(block, iv)
+
+    mode.CryptBlocks(data, data)
+
+    // remove padding
+    trimmed_data := strings.TrimRight(string(data), "=")
+
+    return trimmed_data
+}
+
+
+func generateHMAC(secret_key []byte, timestamp []byte, encrypted_data []byte) []byte {
+    message := append(timestamp, encrypted_data...)
+
+    // create hash mac
+    hash_mac := hmac.New(sha256.New, secret_key)
+    hash_mac.Write(message)
+
+    // return byte array
+    return hash_mac.Sum(nil)
+}
+
+
+func EncryptCookieValue(secret_key []byte, plaintext_data []byte, sep string) string {
+    // generate timestamp
+    now := time.Now()
+    timestamp := []byte(now.Format(time.UnixDate))
+    encoded_timestamp := base64.URLEncoding.EncodeToString(timestamp)
+
+    // generate data blob
+    encrypted_data := encryptData(secret_key, plaintext_data)
+    encoded_data := base64.URLEncoding.EncodeToString(encrypted_data)
+
+    // generate data blob
+    // generate hmac
+    hash_mac := generateHMAC(secret_key, timestamp, encrypted_data)
+    encoded_hash_mac := base64.URLEncoding.EncodeToString(hash_mac)
+
+    // return encoded values
+    return concatValue(encoded_hash_mac, encoded_timestamp, encoded_data, sep)
+}
+
+
+func DecryptCookieValue(secret_key []byte, cookie_value string, sep string) (string, error) {
+    if sep == "" {
+        sep = "."
+    }
+
+    // extract value
+    value := strings.Split(cookie_value, sep)
+    encoded_hash_mac := value[0]
+    encoded_timestamp := value[1]
+    encoded_data := value[2]
+
+    //decode
+    encrypted_data, err := base64.URLEncoding.DecodeString(encoded_data)
+    if err != nil {
+        return "", err
+    }
+
+    timestamp, err := base64.URLEncoding.DecodeString(encoded_timestamp)
+    if err != nil {
+        return "", err
+    }
+
+    returned_hmac, err := base64.URLEncoding.DecodeString(encoded_hash_mac)
+    if err != nil {
+        return "", err
+    }
+
+    // compute and compare
+    computed_hmac := generateHMAC(secret_key, timestamp, encrypted_data)
+
+    if !hmac.Equal(returned_hmac, computed_hmac) {
+        return "", errors.New("Returned HMAC does not match")
+    }
+
+    // hmac mathces, now decrypt data.
+    data := decryptData(secret_key, encrypted_data)
+
+    return string(data), nil
+}
+
---
+++ cookie_test.go
@@ -0,0 +1,139 @@
+package sessioncookie
+
+import (
+    "time"
+    "testing"
+    "crypto/hmac"
+    "encoding/json"
+)
+
+
+type SessionValue struct {
+    Id int
+    Username string
+}
+
+
+const (
+    sep = "||"
+)
+
+
+func setUp() ([]byte, SessionValue, []byte) {
+    key := make([]byte, 32)
+    value := SessionValue{1, "asdf son of asdf"}
+    now := time.Now().Format(time.UnixDate)
+    timestamp := []byte(now)
+    return key, value, timestamp
+}
+
+
+func TestEncryptionDecryption (t *testing.T) {
+    secret_key, value, _ := setUp()
+    plaintext, _ := json.Marshal(value)
+
+    encrypted_value := EncryptCookieValue(secret_key, plaintext, sep)
+    returned_value, _ := DecryptCookieValue(secret_key, encrypted_value, sep)
+
+    if string(plaintext) != returned_value {
+        t.Error("encrypted value does not match decrypted value")
+    }
+}
+
+
+func TestEncryptDecryptData (t *testing.T) {
+    secret_key, value, _ := setUp()
+    plaintext, _ := json.Marshal(value)
+
+    encrypted_data := encryptData(secret_key, plaintext)
+    decrypted_data := decryptData(secret_key, encrypted_data)
+
+    if string(encrypted_data) == decrypted_data {
+        t.Error("encrypted data should not match decrypted data")
+    }
+
+    if decrypted_data != string(plaintext) {
+        t.Error("decrypted data does not match plaintext")
+    }
+}
+
+
+func TestRepeatedEncryption (t *testing.T) {
+    secret_key, value, _ := setUp()
+    plaintext, _ := json.Marshal(value)
+
+    encrypted_data1 := encryptData(secret_key, plaintext)
+    encrypted_data2 := encryptData(secret_key, plaintext)
+
+    if string(encrypted_data1) == string(encrypted_data2) {
+        t.Error("Encryption IV should modify encrypted output")
+    }
+}
+
+
+func TestGenerateHMAC (t *testing.T) {
+    secret_key, value, timestamp := setUp()
+    plaintext, _ := json.Marshal(value)
+
+    encrypted_data := encryptData(secret_key, plaintext)
+    hash_mac1 := generateHMAC(secret_key, timestamp, encrypted_data)
+    hash_mac2 := generateHMAC(secret_key, timestamp, encrypted_data)
+    if !hmac.Equal(hash_mac1, hash_mac2) {
+        t.Error("Same values should generate the same hmac signature")
+    }
+}
+
+
+func TestCompareHMACTimestamp (t *testing.T) {
+    /*
+        Changing timestamp should fail
+        a comparison between generated HMAC signatures
+    */
+
+    secret_key, value, timestamp1 := setUp()
+    plaintext, _ := json.Marshal(value)
+
+    ts := time.Now().Add(time.Duration(time.Second))
+    sts := ts.Format(time.UnixDate)
+    timestamp2 := []byte(sts)
+
+    if string(timestamp1) == string(timestamp2) {
+        t.Error("Timestamps should be different")
+    }
+
+    encrypted_data := encryptData(secret_key, plaintext)
+    hash_mac1 := generateHMAC(secret_key, timestamp1, encrypted_data)
+    hash_mac2 := generateHMAC(secret_key, timestamp2, encrypted_data)
+
+    if hmac.Equal(hash_mac1, hash_mac2) {
+        t.Error("Different timestamps should generate different hmac signature")
+    }
+}
+
+
+func TestCompareHMACData (t *testing.T) {
+    /*
+        Changing encrypted data should fail
+        a comparison between generated HMAC signatures
+    */
+
+    secret_key, value1, timestamp := setUp()
+    plaintext1, _ := json.Marshal(value1)
+
+    value2 := SessionValue{2, "asdf son of asdf"}
+    plaintext2, _ := json.Marshal(value2)
+
+    if string(plaintext1) == string(plaintext2) {
+        t.Error("Data should be different")
+    }
+
+    encrypted_data1 := encryptData(secret_key, plaintext1)
+    encrypted_data2 := encryptData(secret_key, plaintext2)
+
+    hash_mac1 := generateHMAC(secret_key, timestamp, encrypted_data1)
+    hash_mac2 := generateHMAC(secret_key, timestamp, encrypted_data2)
+
+    if hmac.Equal(hash_mac1, hash_mac2) {
+        t.Error("Different data should generate different hmac signature")
+    }
+}
